2f8f089e12c2a50ec557ec42c2ed4cd202a3260b
unknown <vaishnaviknipss023@gmail.com>
2026-08-11 23:12:24 +0530

    Array-revision problems

---
+++ Array/class-1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include<climits>
+using namespace std;
+
+int linearSearch(int arr[],int size, int target) {
+ for(int i=0; i<size; i++) {
+    if(arr[i] == target) {
+        return i;
+}
+  }
+  return -1;
+}
+
+void reverseArr(int arr[],int size) {
+    int start = 0, end = size-1;
+
+    while(start<end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+
+int main() {
+   int arr[] = {4,2,7,8,1,2,5};
+    int size = 7;
+    reverseArr(arr,size);
+
+    for(int i=0; i<size;i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+
+    // int target = 8;
+    // cout<<linearSearch(arr,size,target)<<"";
+
+    // cout<<sizeof(arr)<<endl;
+    // int smallest = INT_MAX;
+    // for(int i=0; i<size;i++) {
+    //     // if(nums[i] < smallest) {
+    //     //     smallest = nums[i];
+    //     // }
+    //       smallest =  min(nums[i],smallest);
+    // }
+    //   cout<<smallest<<endl;
+
+
+    // int largest = INT_MIN;
+
+    // for(int i=0; i<size; i++) {
+    //     largest = max(nums[i],largest);
+    // }
+    // cout<<"lARGEST = "<<largest<<endl;
+   
+
+
+
+    return 0;
+}
