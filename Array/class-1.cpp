782b61307e2e2778628dab578938709e359c0357
unknown <vaishnaviknipss023@gmail.com>
2026-09-01 21:35:32 +0530

    consecutive ones

--- Array/class-1.cpp
+++ Array/class-1.cpp
@@ -171,3 +171,28 @@ int main() {
 
     return 0;
 }
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+          int count = 0;
+          int n = nums.size();
+          int ans = INT_MIN;
+
+          for(int i=0; i<n; i++) {
+            if(nums[i] == 1) {
+                 count++;
+            }
+             else
+            count = 0;
+            ans = max(ans,count);
+           
+          }
+          
+          return ans;
+         
+          
+        };
+        
+    
+};
