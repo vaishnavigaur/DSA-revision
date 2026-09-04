a08d73be1dc780527d0a5af76f76682c769a0546
unknown <vaishnaviknipss023@gmail.com>
2026-09-04 10:13:02 +0530

    Max. subArray sum

--- Array/class-2.cpp
+++ Array/class-2.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
+#include<limits.h>
 using namespace std;
 
-// int maxSubarray(int arr[], int n) {
-    
-// }
+
 
 int main() {
   int arr[5] = {1,2,3,4,5};
    int n = 5;
-//    int ans = maxSubarray(arr, n);
-//    cout<<ans<<endl;
 
-for(int st=0; st<n; st++) {
+
+// for(int st=0; st<n; st++) {
+//         for(int end= st; end<n; end++) {
+//             for(int i=st; i<=end; i++) {
+//                 cout<<arr[i];
+//             }
+//             cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+ int maxSum = INT_MIN;
+    for(int st=0; st<n; st++) {
+        int currSum = 0;
         for(int end= st; end<n; end++) {
-            for(int i=st; i<=end; i++) {
-                cout<<arr[i];
-            }
-            cout<<" ";
+            currSum += arr[end];
+             maxSum = max(currSum, maxSum);
         }
-        cout<<endl;
     }
+    cout<<"Total Max Sum-->"<<maxSum<<endl;
 
     return 0;
 }
