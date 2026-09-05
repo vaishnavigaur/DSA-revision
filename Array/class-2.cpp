f3d96ba337bd6d133dd9da8d12e2eaf386907b4a
unknown <vaishnaviknipss023@gmail.com>
2026-09-05 21:47:26 +0530

    rearrange array element by sign

--- Array/class-2.cpp
+++ Array/class-2.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 
 int main() {
-  int arr[5] = {1,2,3,4,5};
+  int arr[] = {1,2,3,4,5};
    int n = 5;
 
-
+// <-----------------------Max. SubArray--------------------------------
 // for(int st=0; st<n; st++) {
 //         for(int end= st; end<n; end++) {
 //             for(int i=st; i<=end; i++) {
@@ -18,16 +18,53 @@ int main() {
 //         }
 //         cout<<endl;
 //     }
-
- int maxSum = INT_MIN;
+// ----------------------------max subArray Sum----------------------------
+//  int maxSum = INT_MIN;
+//     for(int st=0; st<n; st++) {
+//         int currSum = 0;
+//         for(int end= st; end<n; end++) {
+//             currSum += arr[end];
+//              maxSum = max(currSum, maxSum);
+//         }
+//     }
+//     cout<<"Total Max Sum-->"<<maxSum<<endl;
+// ----------------kadan's Algorithm-------------------------------
+      int maxSum = INT_MIN;
     for(int st=0; st<n; st++) {
         int currSum = 0;
         for(int end= st; end<n; end++) {
             currSum += arr[end];
              maxSum = max(currSum, maxSum);
+           
         }
-    }
-    cout<<"Total Max Sum-->"<<maxSum<<endl;
+          if(currSum<0) {
+            currSum = 0;
+         }
+        
+        cout<<maxSum<<endl;
 
+    }
     return 0;
 }
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int>result(n);
+        int pi = 0;
+        int ni = 1;
+
+        for(int &num:nums) {
+            if(num>0) {
+                result[pi] = num;
+                pi += 2;
+            }else{
+                result[ni] = num;
+                ni += 2;
+            }
+        }
+        return result;
+
+    }
