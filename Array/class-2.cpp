a74053582914bd6aedfff5162c138cf718b8b179
unknown <vaishnaviknipss023@gmail.com>
2026-09-07 05:33:04 +0530

    Next Permutation

--- Array/class-2.cpp
+++ Array/class-2.cpp
@@ -86,3 +86,32 @@ public:
         }
     }
 };
+
+// --------Next permutation-----------
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+       int pivot  = -1, n = nums.size();
+        for(int i=n-2; i>=0; i--) {
+            if(nums[i] < nums[i+1]) {
+                pivot = i;
+                break;
+            }
+        }
+        if(pivot == -1) {
+            reverse(nums.begin(), nums.end());
+            
+            return;
+        }
+        for(int i = n-1; i>pivot; i--) {
+            if(nums[i] > nums[pivot]) {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+        int i=pivot+1, j=n-1;
+         while(i <= j) {
+            swap(nums[i++], nums[j--]);
+         }
+    }
+};
